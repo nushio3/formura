d6c4f3b75d7b86957b65e38850b674487ced3662
nushio <muranushi@gmail.com>
2015-03-27 18:56:22 +0900

    test a type-level tree. Surprisingly it is working well.

---
+++ src/Language/Formura/AST.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE FlexibleInstances, FlexibleContexts, UndecidableInstances, GADTs, StandaloneDeriving, KindSignatures, TemplateHaskell #-}
+module Language.Formura.AST where
+
+import Control.Lens
+import Language.Formura.Parser
+import Text.Trifecta
+import Text.Printf
+
+data Range = Range {_rangeMetadata :: Metadata, _rangeLower :: Rational, _rangeSize :: Int}
+
+instance Show Range where
+  show (Range _ l s) = printf "%s..%s" (pprRational l) (pprRational $ l+fromIntegral s)
+
+data TTree f a where
+  Atom :: a -> TTree f a
+  Cons :: TTree f (f a) -> TTree f a
+
+
+instance (Show a) =>  Show (TTree [] a) where
+  show (Atom x) = show x
+  show (Cons x) = show x
