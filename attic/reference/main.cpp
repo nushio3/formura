2ceefefcf133d204522e9d4131107ab6f833b828
Takayuki Muranushi <muranushi@gmail.com>
2015-05-07 10:13:03 +0900

    debug output

--- attic/reference/main.cpp
+++ attic/reference/main.cpp
@@ -5,11 +5,13 @@
 #include <sys/time.h>
 
 
-const int NX=1024, NY= 1024, N_TIME=1024;
+//const int NX=1024, NY= 1024, N_TIME=1024;
+const int NX=512, NY= 512, N_TIME=512;
 const int X_MASK = NX-1, Y_MASK = NY-1;
 
 const int NS=1;
 const int NT=64;
+const int T_MASK = NT-1;
 const int NG=NT/NS/2; // 32
 const int NTO=NX/NT; // 16
 const int NF=NX/NT*NG; // 16*32 = 512
@@ -88,7 +90,7 @@ double pad[NT+2*NS][NT+2*NS];
 double yslabs[NTO][NTO][NF][NT][2] = {0};
 double xslabs[NTO][NTO][NF][NT][2] = {0};
 double sticks[NTO][NTO][NT][4] = {0};
-double pads[NTO][NTO][NT+2*NS][NT+2*NS] = {0};
+double pads[NTO][NTO][NT][NT] = {0};
 
 #define dens_at_pitch(y,x) pad[(y)][(x)]
 
@@ -96,10 +98,18 @@ void proceed_region
 ( double yslab[NF][NT][2],
   double xslab[NF][NT][2],
   double stick[NF][4],
+  double pad_input[NT][NT],
   double yslab_next[NF][NT][2],
   double xslab_next[NF][NT][2],
-  double stick_next[NF][4]
+  double stick_next[NF][4],
+  double pad_next[NT][NT]
 ) {
+    for(int y=0;y<NT;++y) {
+      for(int x=0;x<NT;++x) {
+        pad[y][x] = pad_input[y][x];
+      }
+    }
+
   for(int t=0; t<NF; ++t){
     for (int i=0;i<NT;++i) {
       pad[i][NT] = yslab[t][i][0];
@@ -127,19 +137,27 @@ void proceed_region
       xslab_next[t][i][0] = pad[0][i]  ;
       xslab_next[t][i][1] = pad[1][i]  ;
     }
-    stick[t][0] = pad[0][0];
-    stick[t][1] = pad[0][1];
-    stick[t][2] = pad[1][0];
-    stick[t][3] = pad[1][1];
+    stick_next[t][0] = pad[0][0];
+    stick_next[t][1] = pad[0][1];
+    stick_next[t][2] = pad[1][0];
+    stick_next[t][3] = pad[1][1];
   }
+  for(int y=0;y<NT;++y) {
+    for(int x=0;x<NT;++x) {
+      pad_next[y][x] = pad[y][x];
+    }
+  }
+
 }
 
 void compute_pitch() {
   for (int large_t=0; large_t < N_TIME/NF; ++large_t) {
-    for (int tile_y=0;tile_y<NTO; ++tile_y) {
-      for (int tile_x=0;tile_x<NTO; ++tile_x) {
-        proceed_region(yslabs[tile_y][tile_x], xslabs[tile_y][tile_x], sticks[tile_y][tile_x],
-                       yslabs[tile_y][tile_x], xslabs[tile_y][tile_x], sticks[tile_y][tile_x]);
+    for (int y5=NTO;y5>=0; --y5) {
+      for (int x5=NTO;x5>=0; --x5) {
+        int x4=(x5-1) & NTO;
+        int y4=(y5-1) & NTO;
+        proceed_region(yslabs[y5][x5], xslabs[y5][x5], sticks[y5][x5],pads[y5][x5],
+                       yslabs[y4][x5], xslabs[y5][x4], sticks[y4][x4],pads[y5][x5]);
       }
     }
   }
@@ -166,6 +184,13 @@ int main(){
 
   dump("test.txt");
 
+  init();
+  for (int y=0;y<NY; ++y) {
+    for (int x=0; x<NX; ++x) {
+      pads[y/NT][x/NT][y&T_MASK][x&T_MASK] = dens[y][x];
+    }
+  }
+
   wct0 = second();
   compute_pitch();
   wct1 = second();
@@ -174,6 +199,12 @@ int main(){
   cerr << flops << " FLOP/s" << endl;
   flops = double(N_TIME/NF)*NTO*NTO*NF*NT*NT*6.0/(wct1-wct0);
   cerr << flops << " FLOP/s" << endl;
-
+  init();
+  for (int y=0;y<NY; ++y) {
+    for (int x=0; x<NX; ++x) {
+      dens[y][x] = pads[y/NT][x/NT][y&T_MASK][x&T_MASK];
+    }
+  }
+  dump("test-pitch.txt");
 
 }
