10741b76e7731e6424a1c1d4fb35528e283630be
nushio <muranushi@gmail.com>
2015-05-21 15:59:30 +0900

    debug

--- attic/reference/body-2d-pitch-simd.cpp
+++ attic/reference/body-2d-pitch-simd.cpp
@@ -1,3 +1,5 @@
+#include <omp.h>
+#include <algorithm>
 string algorithm_tag_str = "PiTCH-SIMD-16";
 
 typedef double v4df __attribute__((vector_size(32)));
@@ -20,6 +22,7 @@ const int N_KABE=NF+NT/2+2;
 double kabe_y[NTO][NTO][N_KABE][2][NT+2];
 double kabe_x[NTO][NTO][N_KABE][NT+2][2];
 
+static __thread
 double work[N_KABE][NT+2][NT+2];
 
 int sim_t_1=-1, sim_t_2=-1;
@@ -32,6 +35,8 @@ void pitch_kernel
  double yuka_in[1][NT+2][NT+2], double kabe_y_in[N_KABE][2][NT+2], double kabe_x_in[N_KABE][NT+2][2],
  double yuka_out[1][NT+2][NT+2], double kabe_y_out[N_KABE][2][NT+2], double kabe_x_out[N_KABE][NT+2][2])
 {
+
+
   for(int t=0; t<NF+NT/4+2;++t) {
     for(int y=0; y<NT+2; ++y) {
       work[t][y][0] = kabe_x_in[t+NT/4][y][0];
@@ -167,8 +172,11 @@ int thread_local_t[NTO];
 int thread_local_yo[NTO];
 int thread_speed_flag[NTO];
 
+int kabe_x_done[99999][NTO][NTO] = {{{0}}};
+int kabe_y_done[99999][NTO][NTO] = {{{0}}};
 
-inline void proceed_thread(int xo) {
+
+void proceed_thread(int xo) {
   if (thread_speed_flag[xo] == 0) return;
 
   int yo = thread_local_yo[xo];
@@ -178,6 +186,11 @@ inline void proceed_thread(int xo) {
   bool near_final   = t_orig >= T_FINAL-NX;
   int y_orig = -t_orig;
   int x_orig = -t_orig;
+
+  if(!near_initial) {
+    assert(  kabe_x_done[NX+t_orig][yo][xo]);
+    assert(  kabe_y_done[NX+t_orig][yo][xo]);
+  }
 
   if(near_initial && near_final) {
     pitch_kernel<true, true>
@@ -219,10 +232,12 @@ inline void proceed_thread(int xo) {
   }
   swap(yuka[yo][xo], yuka_next[yo][xo]);
 
+  kabe_x_done[NX+t_orig+NF][yo][(xo+1)%NTO]=1;
+  kabe_y_done[NX+t_orig+NF][(yo+1)%NTO][xo]=1;
+
 
 
   if(thread_local_t[xo] <= T_FINAL) {
-    //cout << xo << " " << thread_local_yo[xo]<<endl;
     thread_local_yo[xo]++;
     if(thread_local_yo[xo] >= NTO) {
       thread_local_yo[xo]=0;
@@ -262,14 +277,45 @@ void solve(){
   {
     for (;;){
 
-      #pragma omp for
-      for(int xo=0;xo<NTO;xo++) {
-        if(thread_speed_flag[xo]>0) {
-          proceed_thread(xo);
+      int nth = omp_get_num_threads();
+      int tid = omp_get_thread_num();
+
+
+      ostringstream ostr[32];
+//
+// int junjo[NTO];
+// for(int i =0;i<NTO;++i) {
+//   junjo[i]=i;
+// }
+// random_shuffle(junjo, junjo+NTO);
+
+      for(int xo2=0;xo2<NTO;xo2++) {
+        int xo = xo2; //junjo[xo2];
+
+        if (xo%nth == tid) {
+
+          ostr[tid] << xo << " " << thread_local_t[xo] << " " << thread_local_yo[xo] << endl;
+
+          if(thread_speed_flag[xo]>0) {
+            proceed_thread(xo);
+          }
         }
+
+        #pragma omp barrier
+
       }
 
-      #pragma omp barrier
+
+
+      for (int ctr=0;ctr<nth;++ctr) {
+        if(ctr==tid)  cout << ostr[tid].str() << flush;
+        #pragma omp barrier
+      }
+
+
+
+
+
 
       bool flag=true;
       for(int xo=0;xo<NTO;xo++) {
