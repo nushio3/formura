08a16f5317befec57ca4c0c96378bdba4382415a
Takayuki Muranushi <muranushi@gmail.com>
2015-06-17 16:14:12 +0900

    add interactive profiler tool.

---
+++ attic/reference/interactive-prof.sh
@@ -0,0 +1,3 @@
+fipp -C -d ip.simd -Ihwm -Hevent=Instructions_SIMD   ./benchmark-2d-fujitsu.out FIPP
+fipp -C -d ip.mema -Ihwm -Hevent=MEM_access          ./benchmark-2d-fujitsu.out FIPP
+fipp -C -d ip.stat -Ihwm -Hevent=Statistics          ./benchmark-2d-fujitsu.out FIPP
