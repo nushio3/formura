5b85f03911a6fe11649bdc277b7ae3daea2337bc
Takayuki Muranushi <muranushi@gmail.com>
2015-07-22 10:46:05 +0900

    frontend parsing reproduced.

--- exe-src/test-generator.hs
+++ exe-src/test-generator.hs
@@ -7,10 +7,10 @@ import Language.IR.Backend
 
 prog1 :: Function
 prog1 = Function
-  { _name = "chino",
+  { _functionName = "chino",
     _entryDecls = [VarDecl "double" ([-1,-1],[1,1]) "dens"],
     _middleDecls = [VarDecl "double" ([0,-1],[0,1]) "dens_x"],
-    _body = graph1}
+    _functionBody = graph1}
 
 graph1 :: Graph (Insn ()) O O
 graph1 =  mkMiddles
