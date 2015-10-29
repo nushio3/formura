94f283598d0195a9efa058c901cdfcb45bed4f30
Takayuki Muranushi <muranushi@gmail.com>
2015-10-29 13:12:09 +0900

    create new tuple-lang.

--- attic/tuple-lang/Tree.hs
+++ attic/tuple-lang/Tree.hs
@@ -2,9 +2,13 @@
 import Control.Applicative
 import Control.Lens
 import Control.Monad
+import Data.Monoid
+import qualified Data.Set as S
 import Text.Trifecta
-import Metadata
+import Text.Trifecta.Delta (delta)
+import qualified Text.PrettyPrint.ANSI.Leijen as PPR
 
+import Metadata
 
 data TreeWith m a = Leaf m a | Tuple m [TreeWith m a]
                        deriving (Eq,Ord,Show,Read,Functor)
@@ -20,6 +24,7 @@ instance HasMetadata m => HasMetadata (TreeWith m a) where
 pMetadata :: Parser Metadata
 pMetadata = fmap Metadata rend
 
+
 pTree :: Parser a -> Parser (TreeWith Metadata a)
 pTree pElem = pTuple <|> pLeaf
   where
@@ -31,22 +36,21 @@ pTree pElem = pTuple <|> pLeaf
       symbol ")"
       return $ Tuple m children
 
-zipTreeWith :: Metadata -> (a -> b -> c) -> Tree a -> Tree b -> Either String (Tree c)
+zipTreeWith :: Metadata -> (a -> b -> c) -> Tree a -> Tree b -> Either PPR.Doc (Tree c)
 zipTreeWith m f ta tb = case (ta,tb) of
   (Leaf _ a0, Leaf _ b0)  -> return $ Leaf m (f a0 b0)
   (Leaf _ a0, b0@(Tuple _ bs)) -> return $ fmap (f a0) b0
   (a0@(Tuple _ as), Leaf _ b0) -> return $ fmap (flip f b0) a0
-  (Tuple _ as, Tuple _ bs) | length as /= length bs -> Left "Tuple size mismatch"
+  (Tuple ma as, Tuple mb bs) | length as /= length bs -> Left doc0
+    where
+      doc0 :: PPR.Doc
+      doc0 = PPR.text "tuple size mismatch" <> pprm ma <> pprm mb
+
+      pprm :: Metadata -> PPR.Doc
+      pprm (Metadata r1) = explain (addCaret (delta r1) r1) (Err Nothing [] S.empty)
+
   (Tuple _ as, Tuple _ bs) -> Tuple m <$> zipWithM (zipTreeWith m f) as bs
 
 main :: IO ()
 main = do
-  print testTree1
-  print testTree2
-  print $ zipTreeWith undefined (\n xs -> concat $ replicate n xs)testTree1 testTree2
-
-testTree1 :: Tree Int
-testTree1 = undefined -- Leaf 4 -- Tuple [Leaf 3, Tuple[Leaf 4, Leaf 1]]
-
-testTree2 :: Tree String
-testTree2 = undefined -- Tuple [Leaf "hoge", Leaf "M"]
+  putStrLn "HELLOWORLD"
