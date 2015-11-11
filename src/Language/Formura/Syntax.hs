4c7a2f613b60e00eaacd66a982752b446aead03a
Takayuki Muranushi <muranushi@gmail.com>
2015-11-11 15:37:47 +0900

    create the first language construct.

--- src/Language/Formura/Syntax.hs
+++ src/Language/Formura/Syntax.hs
@@ -6,19 +6,35 @@ License     : MIT
 Maintainer  : muranushi@gmail.com
 Stability   : experimental
 
-Components for syntatic elements of formura
+Components for syntatic elements of formura.
 -}
+
+{-# LANGUAGE DeriveFunctor, DeriveFoldable, DeriveGeneric,
+DeriveTraversable, FlexibleContexts, FlexibleInstances,
+PatternSynonyms, ViewPatterns #-}
 
 module Language.Formura.Syntax where
 
+import Control.Lens
+import GHC.Generics
+
+import Language.Combinator
 
 
+-- * Tuple
+
+-- | The functor for tuple.
 data TupleF x = TupleF [x]
-             deriving (Eq, Ord, Functor, Foldable, Traversable)
+             deriving (Eq, Ord, Functor, Foldable, Traversable, Generic)
 instance Show x => Show (TupleF x) where
   show (TupleF xs) = let c '[' = '('
                          c ']' = ')'
                          c x   = x
                      in map c $ show xs
+-- | specialized prism
 tuple :: MatchPrism TupleF
 tuple = match
+
+-- | smart patterns
+pattern Tuple xs <- ((^? tuple) -> Just (TupleF xs)) where
+  Tuple xs = tuple # TupleF xs
