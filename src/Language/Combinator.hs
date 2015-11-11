90601496c07fee6ca04fd1b502cfb47f5460a7c4
Takayuki Muranushi <muranushi@gmail.com>
2015-11-11 13:55:21 +0900

    document.

--- src/Language/Combinator.hs
+++ src/Language/Combinator.hs
@@ -1,3 +1,14 @@
+{-|
+Module      : Language.Combinator
+Description : Short description
+Copyright   : (c) Takayuki Muranushi, 2015
+License     : MIT
+Maintainer  : muranushi@gmail.com
+Stability   : experimental
+
+Combinators for creating a customized language based on Modular Reifiable Matching.
+-}
+
 {-# LANGUAGE ConstraintKinds, DataKinds, DeriveFoldable, DeriveFunctor,
 DeriveTraversable, FlexibleContexts, FlexibleInstances,
 FunctionalDependencies, GADTs, KindSignatures, MultiParamTypeClasses,
@@ -7,47 +18,15 @@ UndecidableInstances, ViewPatterns #-}
 
 module Language.Combinator where
 
-import Control.Applicative
 import Control.Lens
 import Control.Monad
-import Data.Monoid hiding (Sum)
-import qualified Data.Set as S
 import Data.Traversable
 import qualified Text.Trifecta as P hiding (string)
 import qualified Text.Trifecta.Delta as P
-import qualified Text.Parser.Expression as P
-import qualified Text.PrettyPrint.ANSI.Leijen as Ppr hiding (line, (<>), (<$>), empty, integer)
-import System.IO
 
--- | The Compiler Metadata
-data Metadata = Metadata {_metadataRendering :: P.Rendering, _metadataBegin :: P.Delta,  _metadataEnd :: P.Delta}
-makeLenses ''Metadata
+-- * Sum of functors
 
-instance Show Metadata where
-  show = const ""
-instance P.HasRendering Metadata where
-  rendering = metadataRendering
-
-
--- | The fix point of F-algebra, with compiler metadata information
-data Fix f where
-  In :: Functor f => {_metadata :: Maybe Metadata, _out :: f (Fix f)} -> Fix f
-
-instance (Show (f (Fix f))) => Show (Fix f) where
-    showsPrec n (In _ x) = showsPrec n x
-
--- | The lens that accesses the compiler metadata of the syntax tree
-metadata :: Functor f => Lens' (Fix f) (Maybe Metadata)
-metadata fun (In p o) = fmap (\p' -> In p' o) (fun p)
-
-fix :: forall f. Functor f => Iso' (Fix f) (f (Fix f))
-fix = iso _out go
-  where
-    go :: f (Fix f) -> Fix f
-    go ffixf = In Nothing ffixf
-
-
--- | The sum of functors
+-- | The datatype for the sum of functors.
 data Sum (fs :: [* -> *]) x where
   Void :: Sum '[] x
   Here :: Traversable f => f x -> Sum (f ': fs) x
@@ -73,13 +52,18 @@ instance Traversable (Sum fs) where
   traverse afb (Here x)  = Here  <$> traverse afb x
   traverse afb (There x) = There <$> traverse afb x
 
--- The prisms for accessing here and there
+instance Elem f fs => Matches f (Sum fs x) where
+  type Content f (Sum fs x) = x
+  match = constructor
+
+-- | The prisms for accessing the first functor in the Sum
 _Here :: Traversable f => Prism' (Sum (f ': fs) x) (f x)
 _Here = let a :: Sum (f ': fs) x -> Maybe (f x)
             a (Here x) = Just x
             a _        = Nothing
     in prism' Here a
 
+-- | The prisms for accessing the rest of functors in the Sum
 _There :: Traversable f => Prism' (Sum (f ': fs) x) (Sum fs x)
 _There = let a :: Sum (f ': fs) x -> Maybe (Sum fs x)
              a (There x) = Just x
@@ -87,7 +71,8 @@ _There = let a :: Sum (f ': fs) x -> Maybe (Sum fs x)
     in prism' There a
 
 
--- The constraint that functor f is an element of a functor set fs
+
+-- | The constraint that functor f is an element of 'Sum' fs
 class Elem f fs where
   constructor :: Prism' (Sum fs x) (f x)
 
@@ -96,7 +81,7 @@ instance {-# OVERLAPPING #-} Traversable f => Elem f (f ': fs) where
 instance {-# OVERLAPPABLE #-} (Traversable f, Traversable g, Elem f fs) => Elem f (g ': fs) where
   constructor = _There . constructor
 
--- The constraint that set of functors fs is a subset of gs
+-- | The constraint that set of functors @fs@ is a subset of @gs@
 class Subset fs gs where
   subrep :: Prism' (Sum gs x) (Sum fs x)
 
@@ -117,74 +102,146 @@ instance {-# OVERLAPPABLE #-} (Traversable f, Elem f gs, Subset fs gs) => Subset
                bwd _                     = Nothing
            in prism' fwd bwd
 
--- The constraint that object x can somehow be matched to functor f
+
+-- * Tools for matching
+
+-- | The constraint that object @x@ can somehow be matched to functor @f@; there is a 'Prism'' from type @x@
+--   to type @f (Content f x)@.
 class Matches f x where
   type Content f x :: *
   match :: Prism' x (f (Content f x))
+
+-- | The type of the  'Prism'' that matches any @x@ such that @Matches f x@.
+type MatchPrism (f :: * -> *) = Matches f x => Prism' x (f (Content f x))
+
 
 instance Matches f (f x) where
   type Content f (f x) = x
   match = simple
 
-instance Elem f fs => Matches f (Sum fs x) where
-  type Content f (Sum fs x) = x
-  match = constructor
+-- * Syntax tree
+
+-- | The compiler metadata.
+data Metadata = Metadata {_metadataRendering :: P.Rendering, _metadataBegin :: P.Delta,  _metadataEnd :: P.Delta}
+makeLenses ''Metadata
+
+instance Show Metadata where
+  show = const ""
+instance P.HasRendering Metadata where
+  rendering = metadataRendering
+
+
+-- | The fix point of F-algebra, with compiler metadata information. This is the datatype we use to represent any AST.
+data Fix f where
+  In :: Functor f => {_metadata :: Maybe Metadata, _out :: f (Fix f)} -> Fix f
+
+instance (Show (f (Fix f))) => Show (Fix f) where
+    showsPrec n (In _ x) = showsPrec n x
 
 instance Elem f fs => Matches f (Fix (Sum fs)) where
   type Content f (Fix (Sum fs)) = Fix (Sum fs)
   match = fix . constructor
 
--- emulate some mrm
-type MRM_Matches fs a b = Sum fs a -> b
 
-extractAt :: Elem f fs => (MRM_Matches fs a b) -> (f a -> b)
-extractAt sfun fa = sfun $ (review constructor) fa
 
--- We create languages by folding over set of functors
+-- | The lens that accesses the compiler metadata of the syntax tree
+metadata :: Functor f => Lens' (Fix f) (Maybe Metadata)
+metadata fun (In p o) = fmap (\p' -> In p' o) (fun p)
+
+-- | The lens to convert to/from 'Fix' and its content.
+
+fix :: forall f. Functor f => Iso' (Fix f) (f (Fix f))
+fix = iso _out go
+  where
+    go :: f (Fix f) -> Fix f
+    go ffixf = In Nothing ffixf
+
+
+-- * Syntax tree utility
+
+
+
+-- | Languages are 'Fix' over 'Sum' of functors
 type Lang (fs :: [ * -> * ]) = Fix (Sum fs)
-type MatchPrism (f :: * -> *) = forall f x . Matches f x => Prism' x (f (Content f x))
 
-type Algebrogen f a b = f a -> b
+
+-- | An F-algebra.
 type Algebra f a = f a -> a
+
+-- | A monadic F-algebra.
 type AlgebraM m f a = f a -> m a
 
-foldout :: Algebra f a -> Fix f -> a
-foldout k (In _ x) = k $ fmap (foldout k) x
+-- | A precursor for an 'Algebra' .
+type Algebrogen f a b = f a -> b
+
+
+-- | The catamorphism that is specialized to 'Lang' . It copies the metadata from @Lang f@ to @Lang g@.
 
 fold :: Algebra f (Lang g) -> Fix f -> (Lang g)
-fold k (In meta x) = (k $ fmap (fold k) x) & metadata .~ meta
+fold k (In meta x) = propagateMetadata meta $ k $ fmap (fold k) x
 
-
-mlift :: (Monad m, Traversable fs) => Algebrogen fs a b -> Algebrogen fs (m a) (m b)
-mlift fsa2b fsma = liftM fsa2b $ sequence fsma
+-- | Monadic 'fold' .
 
 mfold :: (Monad m, Traversable f) => AlgebraM m f (Lang g) -> Fix f -> m (Lang g)
 mfold k (In meta x) = do
   r1 <- traverse (mfold k) x
   r2 <- k r1
-  return $ r2 & metadata .~ meta
+  return $ propagateMetadata meta r2
+
+-- | Propagate Metadata from the top of the syntax tree, in case the algebra had added more than one constructors.
+
+propagateMetadata :: Maybe Metadata -> Lang f -> Lang f
+propagateMetadata Nothing x = x
+propagateMetadata (Just meta) x = go x
+  where
+    go (In Nothing y) = In (Just meta) $ fmap go y
+    go y = y
+
+
+-- | Lift an 'Algebrogen' to monad.
+
+mlift :: (Monad m, Traversable fs) => Algebrogen fs a b -> Algebrogen fs (m a) (m b)
+mlift fsa2b fsma = liftM fsa2b $ sequence fsma
+
+-- | A generic catamorphism, where the compiler metadata is lost.
+
+foldout :: Algebra f a -> Fix f -> a
+foldout k (In _ x) = k $ fmap (foldout k) x
+
+-- | Monadic 'foldout' .
 
 mfoldout :: Monad m => (Sum fs a -> m a) -> Lang fs -> m a
 mfoldout k x = foldout (join . mlift k) x
 
+-- | Promote a @Lang fs@ to @Lang gs@, when @gs@ has more constructors than @fs@.
+
 subFix :: (Subset fs gs) => Lang fs -> Lang gs
 subFix = fold (review (fix . subrep))
+
+-- | Restrict a function from @Lang gs@ to that from @Lang fs@, where @fs@ has less constructors than @gs@.
 
 subOp :: (Subset fs gs) => (Lang gs -> c) -> Lang fs -> c
 subOp g = g . subFix
 
+-- | An algebra that just copies what found in @Lang fs@ to @Lang gs@.
+
 transAlg :: Subset fs gs => Algebra (Sum fs) (Lang gs)
 transAlg = review (fix . subrep)
+
+-- | A monadic 'transAlg' .
 
 mTransAlg :: (Monad m, Subset fs gs) => AlgebraM m (Sum fs) (Lang gs)
 mTransAlg = return . transAlg
 
+-- | Cons an algebra to a 'Sum' of an algebra, to create a larger algebra.
 
 (+::) :: Algebrogen f a b -> Algebrogen (Sum fs) a b -> Algebrogen (Sum (f ': fs)) a b
 af +:: afs = affs
   where
     affs (Here x)  = af  x
     affs (There x) = afs x
+
+-- | Override a specific algebra @f@ in an algebra over @fs@.
 
 (>::) :: Elem f fs => Algebra f a -> Algebra (Sum fs) a -> Algebra (Sum fs) a
 af >:: afs= affs
