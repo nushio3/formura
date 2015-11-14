2fc3b900f2ce13127aa0c8d97c7b9ad957ce049a
Takayuki Muranushi <muranushi@gmail.com>
2015-11-14 22:24:01 +0900

    wrote parser

--- exe-src/formura.hs
+++ exe-src/formura.hs
@@ -1,14 +1,19 @@
 {-# LANGUAGE DataKinds #-}
 module Main where
 
-import Formura.Language.Combinator
+import qualified Text.Trifecta as P
+
 import Formura.Syntax
-
-import qualified Test.QuickCheck as Q
-
-type TestExpr  = Lang '[ImmF , TupleF, OperatorF]
+import qualified Formura.Parser as P
 
 main :: IO ()
 main = do
-  x <- Q.generate Q.arbitrary
-  print (x :: TestExpr)
+  argv <- getArgs
+  mapM_ process argv
+
+process :: FilePath -> IO ()
+process fn = do
+  mprog <- P.parseFromFile P.program fn
+  case mprog of
+      Right prog -> print $ prog
+      Left doc -> Ppr.displayIO stdout $ Ppr.renderPretty 0.8 80 $ doc <> Ppr.linebreak
