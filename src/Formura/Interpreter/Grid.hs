729fad23962c945e6b5a004a251933387a60e858
Takayuki Muranushi <muranushi@gmail.com>
2015-11-16 19:53:18 +0900

    introduce grid semantics.

--- src/Formura/Interpreter/Grid.hs
+++ src/Formura/Interpreter/Grid.hs
@@ -1,3 +1,11 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Formura.Interpreter.Grid where
 
+import           Control.Lens
 import qualified Data.Vector as V
+
+data GridValue a =
+  GridValue
+  { _content :: V.Vector a
+    _offset  :: [Rational]
+  }
