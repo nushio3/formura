58a21da5c3ea6e153be0a18ac43469c990eb480d
Takayuki Muranushi <muranushi@gmail.com>
2015-11-27 14:30:15 +0900

    wrote show, read, num instance.

--- src/Formura/Vec.hs
+++ src/Formura/Vec.hs
@@ -12,15 +12,28 @@ ZipList treated as mathematical vectors, to deal with multidimensionality in ste
 module Formura.Vec where
 
 import Control.Applicative
-
+import Text.Read
 
 newtype Vec a = Vec { getVec :: [a] }
-              deriving (Show, Eq, Ord, Read, Functor, Foldable, Traversable)
+              deriving (Eq, Ord, Functor, Foldable, Traversable)
+
+instance Show a => Show (Vec a) where
+  showsPrec n (Vec xs) = showsPrec n xs
+
+instance Read a => Read (Vec a) where
+  readPrec = Vec <$> readPrec
+
+
 
 instance Applicative Vec where
     pure x = Vec (repeat x)
     Vec fs <*> Vec xs = Vec (zipWith id fs xs)
 
 instance Num a => Num (Vec a) where
-  (+) = pure (+)
-  (*) = pure (*)
+  a + b = (+) <$> a <*> b
+  a - b = (-) <$> a <*> b
+  a * b = (*) <$> a <*> b
+  abs   = fmap abs
+  signum = fmap signum
+  negate = fmap negate
+  fromInteger = pure . fromInteger
