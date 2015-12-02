5c4a20d62e0e56b56d548c98e5535c9fd888a94d
nushio <muranushi@gmail.com>
2015-12-02 19:03:04 +0900

    understood the avx comprehension.

--- attic/mm256_test/basic-avx.cpp
+++ attic/mm256_test/basic-avx.cpp
@@ -4,20 +4,27 @@
 
 using namespace std;
 
+void inspect(__m256 a) {
+  float dest[8];
+  _mm256_storeu_ps(&dest[0], a);
+  for (int i = 0; i < 8; ++i)
+    cout << dest[i] << "\t";
+  cout << endl;
+
+}
+
 int main () {
   float src_a[8], dest[8];
   for (int i = 0; i < 8; ++i)
-    src_a[i] = float(i);
+    src_a[i] = float(i+1);
   __m256 a,b,c;
   a = _mm256_loadu_ps(&src_a[0]);
   b = _mm256_set1_ps(100.0f);
-  __m256 mask;
-  c = a*b-a;
-  mask = _mm256_cmp_ps(c,_mm256_set1_ps(500.0f) , _CMP_LT_OQ);
-  c = _mm256_blendv_ps(c,-c,mask);
-  c =  _mm256_set1_ps(1.0f) / c;
-  _mm256_storeu_ps(&dest[0], c);
-  for (int i = 0; i < 8; ++i)
-    cout << dest[i] << endl;
+  c = a*b+2.0f*a;
+  inspect(c);
+  __m256 d = _mm256_permutevar8x32_ps(c, _mm256_set_epi32(0,7,6,5,4,3,2,1));
+  inspect(d);
+  __m256 e = _mm256_permutevar8x32_ps(c, _mm256_set_epi32(6,5,4,3,2,1,0,7));
+  inspect(e);
   return 0;
 }
