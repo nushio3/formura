4c4f79b71b6bb60475b2a9313d822a129e91d471
Takayuki Muranushi <muranushi@gmail.com>
2015-12-02 22:46:05 +0900

    update the test code.

--- attic/mm256_test/host.cpp
+++ attic/mm256_test/host.cpp
@@ -8,41 +8,40 @@ using namespace std;
 const __m256i permute_fwd =  _mm256_set_epi32(0,7,6,5,4,3,2,1);
 const __m256i permute_bwd =  _mm256_set_epi32(6,5,4,3,2,1,0,7);
 
-const int NX_AVX = 4;
+const int NX_AVX = 8;
 const int NX = 8 * NX_AVX;
 
 void inspect(__m256 *a) {
   float dest[8];
-  for (int j = 0; j<NX_AVX; ++j){
-    _mm256_storeu_ps(&dest[0], a[j]);
-    for (int i = 0; i < 8; ++i)
+  for (int i = 0; i < 8; ++i){
+    for (int j = 0; j<NX_AVX; ++j){
+      _mm256_storeu_ps(&dest[0], a[j]);
       cout << dest[i];
+    }
   }
   cout << endl;
 }
 
-#define SWAP swap(a_0,b_next)
+#define SWAP swap(a_0,a_next)
 
 
 __m256 state[NX_AVX];
 __m256 state_next[NX_AVX];
 
-__m256 a[NX_AVX];
+__m256 b[NX_AVX];
 __m256 c[NX_AVX];
 
 int main(){
 
-  __m256 *a_0 = state, *b_next = state_next;
+  __m256 *a_0 = state, *a_next = state_next;
 
-  for (int i = 1; i < NX_AVX; ++i) {
+  for (int i = 0; i < NX_AVX; ++i) {
     state[i] = _mm256_set_ps(0,0,0,0,0,0,0,0);
   }
-  state[0] = _mm256_set_ps(0,0,0,0,0,0,0,1);
-  
-  inspect(state);
-  for (int t=0;t<4;++t) {
+  state[0] = _mm256_set_ps(0,0,0,1,0,0,0,0);
+
+  for (int t=0;t<32;++t) {
+    inspect(a_0);
 #include "loop-body.cpp"
   }
-  inspect(state);
 }
-
