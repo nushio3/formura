30a39885e844ce72b8c12e4f781fde3b71f4f30d
Takayuki Muranushi <muranushi@gmail.com>
2016-01-05 12:04:34 +0900

    use multi-thread mpi.

--- attic/mpi-test/src/ping-pong.cpp
+++ attic/mpi-test/src/ping-pong.cpp
@@ -337,7 +337,13 @@ void free_pipes() {
 }
 
 int main(int argc, char **argv){
-  MPI_Init(&argc, &argv);
+  int provided;
+  MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
+  if (provided < MPI_THREAD_MULTIPLE) {
+    cerr << "Uwa, hikusugi? :" << provided <<endl;
+    MPI_Abort(MPI_COMM_WORLD, 1);
+  }
+
   MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
   MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
 
