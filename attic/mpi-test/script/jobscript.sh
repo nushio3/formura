337763bab107bd94a1208e5ceb43f626cd24936c
a03209 <a03209@fe01p07.k>
2016-01-05 12:40:04 +0900

    script for submitting jobs.

---
+++ attic/mpi-test/script/jobscript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+#
+#PJM --rsc-list "node=12"
+#PJM --rsc-list "elapse=00:05:00"
+#PJM --stg-transfiles all
+#PJM --stgin "./bin/ping-pong ./bin/ping-pong"
+#PJM -s
+#
+
+. /work/system/Env_base
+mpiexec -n 12 ./bin/ping-pong
