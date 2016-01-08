d900b626c62165a744ada0e949c480b126b87712
a03209 <a03209@fe01p06.k>
2016-01-08 14:36:57 +0900

    add files

---
+++ attic/mpi-test/src/pof-2z.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <fj_tool/fapp.h>
+#define NX 64
+#define NY 64
+#define NZ 64
+
+#define T_MAX 100000
+
+double aa[NZ][NY][NX];
+double ab[NZ][NY][NX];
+double ac[NZ][NY][NX];
+double ad[NZ][NY][NX];
+
+
+
+double wctime() {
+  struct timeval tv;
+  gettimeofday(&tv,NULL);
+  return (double)tv.tv_sec + (double)tv.tv_usec*1e-6;
+}
+
+
+int main () {
+
+  for (int k=0;k<NZ;++k) {
+    for (int j=0;j<NY;++j) {
+      for (int i=0;i<NX;++i) {
+        aa[k][j][i] = rand()/(double)RAND_MAX;
+        ab[k][j][i] = rand()/(double)RAND_MAX;
+        ac[k][j][i] = rand()/(double)RAND_MAX;
+        ad[k][j][i] = rand()/(double)RAND_MAX;
+      }
+    }
+  }
+
+  double t_begin = wctime();
+  start_collection("region");
+
+#define LOOPBODY(PRE_HOOK,POST_HOOK)	 \
+  for (int j=0;j<NY;++j) {		 \
+    for (int i=0;i<NX;++i) {		 \
+    double x = aa[k][j][i];		 \
+					 \
+    PRE_HOOK;				 \
+					 \
+    double x0 = 0.890*x - 0.880*x*x;	 \
+    double x1 = 0.889*x - 0.881*x*x;	 \
+    double x2 = 0.888*x - 0.882*x*x;	 \
+					 \
+    x0 = 3.89*x0 - 3.88*x0*x0;		 \
+    x1 = 3.89*x1 - 3.88*x1*x1;		 \
+    x2 = 3.89*x2 - 3.88*x2*x2;		 \
+					 \
+    x0 = 3.89*x0 - 3.88*x0*x0;		 \
+    x1 = 3.89*x1 - 3.88*x1*x1;		 \
+    x2 = 3.89*x2 - 3.88*x2*x2;		 \
+					 \
+    x0 = 3.89*x0 - 3.88*x0*x0;		 \
+    x1 = 3.89*x1 - 3.88*x1*x1;		 \
+    x2 = 3.89*x2 - 3.88*x2*x2;		 \
+					 \
+    x0 = 3.89*x0 - 3.88*x0*x0;		 \
+    x1 = 3.89*x1 - 3.88*x1*x1;		 \
+    x2 = 3.89*x2 - 3.88*x2*x2;		 \
+					 \
+    x0 = 3.89*x0 - 3.88*x0*x0;		 \
+    x1 = 3.89*x1 - 3.88*x1*x1;		 \
+    x2 = 3.89*x2 - 3.88*x2*x2;		 \
+					 \
+					 \
+    aa[k][j][i] = (x0*x1+x2)/2;		 \
+					 \
+    POST_HOOK;				 \
+    }					 \
+  }
+
+
+  for (int t=0;t<T_MAX; ++t){
+    {
+      const int k = 0;
+      LOOPBODY(x=ab[k][j][i],	{});
+    }
+    for (int k=1;k<NZ-1;++k) {
+      LOOPBODY({},{});
+    }
+    {
+      const int k = NZ-1;
+      LOOPBODY({}, ab[k][j][i] *= x1);
+    }
+  }
+
+  stop_collection("region");
+  double t_end = wctime();
+  double flops = (6 * 12 + 3.0)*(double)NX*NY*NZ*T_MAX / (t_end - t_begin);
+
+  double sum = 0;
+  for (int k=0;k<NZ;++k) {
+    for (int j=0;j<NY;++j) {
+      for (int i=0;i<NX;++i) {
+        sum += aa[k][j][i] + ab[k][j][i] + ac[k][j][i] + ad[k][j][i];
+      }
+    }
+  }
+
+  printf("sum = %lf, wctime = %lf sec\n", sum, t_end - t_begin);
+  printf("%lf GFlop/s\n", flops/1e9);
+}
