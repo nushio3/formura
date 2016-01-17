5b4cd3185a3d7fb729ae4a19e53fc2df3d56fb96
Takayuki Muranushi <muranushi@gmail.com>
2016-01-17 19:20:54 +0900

    implement geometry

--- src/Formura/Geometry.hs
+++ src/Formura/Geometry.hs
@@ -2,7 +2,9 @@
 
 module Formura.Geometry where
 
-import Control.Monad.IO.Class ()
+import Control.Lens
+import Control.Monad
+import Control.Monad.IO.Class
 import "monads-tf" Control.Monad.Reader
 import Formura.GlobalEnvironment
 import Formura.Vec
@@ -11,10 +13,59 @@ import Data.SBV
 type SInt = SInt32
 type Pt = Vec SInt
 
-class (HasGlobalEnvironment (EnvType m), MonadReader m, MonadIO m) => MonadGeometry r m | m -> r
+doesProve :: (Provable a, MonadGeometry m) =>  a -> m Bool
+doesProve thm = do
+  result <- liftIO $ prove thm
+  if (not $ modelExists result)
+    then return True
+    else return False
+
+
+
+class (HasGlobalEnvironment (EnvType m), MonadReader m, MonadIO m) => MonadGeometry m
 
 newtype Body = Body (Pt -> SBool)
 
-data Orthotope = Vec (Int,Int)
-
+type Orthotope = Vec (Int,Int)
 newtype Compound = Compound [Orthotope]
+
+class HasPredicate a where
+  toPredicate :: a -> (Pt -> SBool)
+
+instance HasPredicate Body where
+  toPredicate (Body p) = p
+
+range :: (Int, Int) -> (SInt -> SBool)
+range (lo,hi) x = lo' .<= x &&& x .< hi'
+  where
+    lo' = fromIntegral lo
+    hi' = fromIntegral hi
+
+-- TODO: newtype this range.
+instance Num (Int, Int) where
+  fromInteger n = (fromInteger n,fromInteger n)
+
+instance HasPredicate Orthotope where
+  toPredicate vRange vx = case liftVec2 range vRange vx of
+    PureVec b -> b
+    Vec bs -> bAnd bs
+
+class HasCompound a where
+  toCompound :: MonadGeometry m => a -> m Compound
+
+instance HasCompound Compound where
+  toCompound = return
+
+instance HasCompound Body where
+  toCompound (Body p0) = do
+    iNames <- view axesNames
+    let loNames = map ("lo_" ++) iNames
+        hiNames = map ("hi_" ++) iNames
+        problem0 :: Symbolic SBool
+        problem0 = do
+          loVars <- mapM exists loNames
+          hiVars <- mapM exists hiNames
+          ptVars <- mapM forall iNames
+          sequence_ [constrain $ (l .<= i &&& i .< (h::SInt)) | (l,h,i)<-zip3 loVars hiVars ptVars]
+          return true
+    return $ Compound []
