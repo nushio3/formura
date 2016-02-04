d79f0dddb898d9edc81a3166f8c850faaf8cf942
Takayuki Muranushi <muranushi@gmail.com>
2016-02-04 16:27:25 +0900

    add manifestation module files.

---
+++ src/Formura/OrthotopeMachine/Manifestation.hs
@@ -0,0 +1,13 @@
+{- |
+Copyright   : (c) Takayuki Muranushi, 2015
+License     : MIT
+Maintainer  : muranushi@gmail.com
+Stability   : experimental
+
+
+A module for manifestation of the Orthotope Machine: that is, an operation that removes all the delayed nodes, and replace all shift instructions with cursored-load at manifest variables.
+-}
+
+{-# LANGUAGE DataKinds, DeriveFunctor, DeriveFoldable, DeriveTraversable, FlexibleInstances, PatternSynonyms,TemplateHaskell, TypeSynonymInstances, ViewPatterns #-}
+
+module Formura.OrthotopeMachine.Manifestation where
