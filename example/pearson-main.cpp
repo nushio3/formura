e4c926fdb7c7cdd07cda9e0a7f2848eec9bba59d
Takayuki Muranushi <muranushi@gmail.com>
2016-02-09 11:03:07 +0900

    start automake scheme.

--- example/pearson-main.cpp
+++ example/pearson-main.cpp
@@ -1,13 +1,38 @@
 #include <stdio.h>
 #include <mpi.h>
-#include "diffusion.h"
+#include <stdlib.h>
+#include "pearson.h"
 
 const int T_MAX = 100;
 
+Formura_Navigator navi;
+
+float frand() {
+  return rand() / float(RAND_MAX);
+}
+
+void init() {
+  for(int y = navi.lower_y; y < navi.upper_y; ++y) {
+    for(int x = navi.lower_x; x < navi.upper_x; ++x) {
+      U[y][x] = 1;
+      V[y][x] = 0;
+    }
+  }
+  for (int y = 118; y < 138; ++y) {
+    for (int x = 118; x < 138; ++x) {
+      U[y][x] = 0.5+0.01*frand();
+      V[y][x] = 0.25+0.01*frand();
+    }
+  }
+}
+
 int main (int argc, char **argv) {
-  Formura_Navigator navi;
+  srand(time(NULL));
   MPI_Init(&argc, &argv);
   Formura_Init(&navi, MPI_COMM_WORLD);
+
+  init();
+
   while(navi.time_step < T_MAX) {
     Formura_Forward(&navi);
     printf("t = %d\n", navi.time_step);
