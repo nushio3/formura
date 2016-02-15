a2e6b97f40518e7258401af7e0f09cebe9e21421
Takayuki Muranushi <muranushi@gmail.com>
2016-02-15 15:29:45 +0900

    Gowappper

---
+++ examples/diffusion-short.cpp
@@ -0,0 +1,15 @@
+#include <mpi.h>
+#include "diffusion-short.h"
+
+
+
+
+int Formura_Init(Formura_Navigator * navi, MPI_Comm comm)
+{
+}
+
+
+int Formura_Forward(Formura_Navigator * navi)
+{
+  printf("mae ni susumou!");
+}
