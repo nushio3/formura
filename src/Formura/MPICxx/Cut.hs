28034bf6ceed6bc1dac2c07ab62bab7f7c9379c2
Takayuki Muranushi <muranushi@gmail.com>
2016-02-17 10:29:29 +0900

    Start implementing plan

--- src/Formura/MPICxx/Cut.hs
+++ src/Formura/MPICxx/Cut.hs
@@ -1,1 +1,18 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Formura.MPICxx.Cut where
+
+import Control.Lens
+
+import Formura.Vec
+import Formura.Geometry
+import Formura.OrthotopeMachine.Graph
+
+newtype MPIRank = MPIRank (Vec Int) deriving (Eq, Ord, Show, Read)
+
+data MPIPlan = MPIPlan
+
+
+
+cut :: MMGraph -> MPIPlan
+cut = undefined
