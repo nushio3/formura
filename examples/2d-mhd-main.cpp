c724550d1c94c94f923f555c53e1d8bdf51d7cbe
Takayuki Muranushi <muranushi@gmail.com>
2016-02-23 18:24:08 +0900

    Implement acceptable driver

--- examples/2d-mhd-main.cpp
+++ examples/2d-mhd-main.cpp
@@ -2,9 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "pearson.h"
+#include "2d-mhd.h"
 
 const int T_MAX=1000;
+
+Formura_Navigator navi;
 
 float frand() {
   return rand() / float(RAND_MAX);
@@ -25,7 +27,29 @@ void init() {
 
 
 
-int main ()
+int main (int argc, char **argv)
 {
+  srand(time(NULL));
+  MPI_Init(&argc, &argv);
+  Formura_Init(&navi, MPI_COMM_WORLD);
 
+  init();
+
+  while(navi.time_step < T_MAX) {
+    if(navi.time_step % 100 == 0) {
+      printf("t = %d\n", navi.time_step);
+      char fn[256];
+      sprintf(fn, "frames/%06d.txt", navi.time_step);
+      FILE *fp = fopen(fn,"w");
+      for(int x = navi.lower_x; x < navi.upper_x; ++x) {
+        for(int y = navi.lower_y; y < navi.upper_y; ++y) {
+          fprintf(fp, "%d %d %f\n", x, y, dens[x][y]);
+        }
+        fprintf(fp, "\n");
+      }
+      fclose(fp);
+    }
+    Formura_Forward(&navi);
+  }
+  MPI_Finalize();
 }
