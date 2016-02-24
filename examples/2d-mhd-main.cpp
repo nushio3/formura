6333f082b8adb89d19522bcecbb21a343d7102e9
Takayuki Muranushi <muranushi@gmail.com>
2016-02-24 17:05:09 +0900

    With MHD: has bug on the corner case

--- examples/2d-mhd-main.cpp
+++ examples/2d-mhd-main.cpp
@@ -20,14 +20,14 @@ void init() {
   for(int ix = navi.lower_x; ix < navi.upper_x; ++ix) {
     for(int iy = navi.lower_y; iy < navi.upper_y; ++iy) {
       double x = ix/(double)NX, y = iy/(double)NY;
-      dens[ix][iy] = 0;
-      vx[ix][iy]   = -sin(2 * PI * y);
-      vy[ix][iy]   =  sin(2 * PI * x);
-      vz[ix][iy]   = 0;
-      Bx[ix][iy]   = -sin(2 * PI * y);
-      By[ix][iy]   =  sin(4 * PI * x);
-      Bz[ix][iy]   = 0;
-      s[ix][iy]    = 0;
+      dens[ix][iy] = 1; // 0;
+      vx[ix][iy]   = 0; // -sin(2 * PI * y);
+      vy[ix][iy]   = 0; //  sin(2 * PI * x);
+      vz[ix][iy]   = 0; // 0;
+      Bx[ix][iy]   = 0; // -sin(2 * PI * y);
+      By[ix][iy]   = 0; //  sin(4 * PI * x);
+      Bz[ix][iy]   = 0; // 0;
+      s[ix][iy]    = 0; // 0;
     }
   }
 }
@@ -46,7 +46,7 @@ int main (int argc, char **argv)
     if(navi.time_step % 10 == 0) {
       printf("t = %d\n", navi.time_step);
       char fn[256];
-      sprintf(fn, "out-2d-mhd/%06d.txt", navi.time_step);
+      sprintf(fn, "out-2d-mhd/dens-%06d.txt", navi.time_step);
       FILE *fp = fopen(fn,"w");
       for(int x = navi.lower_x; x < navi.upper_x; ++x) {
         for(int y = navi.lower_y; y < navi.upper_y; ++y) {
@@ -55,6 +55,18 @@ int main (int argc, char **argv)
         fprintf(fp, "\n");
       }
       fclose(fp);
+
+
+      sprintf(fn, "out-2d-mhd/vx-%06d.txt", navi.time_step);
+      fp = fopen(fn,"w");
+      for(int x = navi.lower_x; x < navi.upper_x; ++x) {
+        for(int y = navi.lower_y; y < navi.upper_y; ++y) {
+          fprintf(fp, "%d %d %f\n", x, y, vx[x][y]);
+        }
+        fprintf(fp, "\n");
+      }
+      fclose(fp);
+
     }
     Formura_Forward(&navi);
   }
---
+++ scripts/plot-2d-mhd.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import sys
+import subprocess
+
+# ident = sys.argv[1]
+
+fns = sys.argv
+for fn in fns[1:]:
+    print fn
+    fn_out = fn.replace('.txt', '.png')
+    fn_in = fn.replace('.txt', '.tmp')
+    with open(fn,'r') as fp:
+        with open(fn_in,'w') as fp2:
+            con = fp.read()
+            fp2.write(con.replace('nan','0'))
+    with open('scripts/plot.gnu', 'w') as fp:
+        fp.write("""
+        set term png
+        set pm3d
+        set pm3d map
+        set size ratio -1
+        set cbrange [-1:1]
+        set out "{fn_out}"
+        splot "{fn_in}"
+""".format(fn_in=fn_in, fn_out=fn_out))
+
+    subprocess.call("gnuplot scripts/plot.gnu", shell=True)
