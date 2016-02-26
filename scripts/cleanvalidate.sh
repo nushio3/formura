4886bd94fad1e95214f23c41501ed6a837a4ecb2
Takayuki Muranushi <muranushi@gmail.com>
2016-02-26 17:49:29 +0900

    Update cleanbuild script

--- scripts/cleanvalidate.sh
+++ scripts/cleanvalidate.sh
@@ -5,4 +5,7 @@ stack build
 ./cmake-for-x86.sh
 stack exec formura -- examples/2d-mhd.fmr
 make
+mkdir -p out-2d-mhd
+rm out-2d-mhd/*
 ./bin/2d-mhd.out 5000 100
+./scripts/plot-2d-mhd.py out-2d-mhd/*.txt
