bec1246729864068b567ef11be6e94ce03911918
Takayuki Muranushi <muranushi@gmail.com>
2016-03-02 10:44:04 +0900

    Activate 3d mhd

--- scripts/test-on-k.py
+++ scripts/test-on-k.py
@@ -53,6 +53,6 @@ mpirun -n 4 ./a.out
 cmd('chmod 755 '+submit_script_path)
 
 cmd('scp {}/*  {}:{}'.format(tmpdir, host,tmpdir))
-on_k('mpiFCCpx 2d-mhd*.c 2d-mhd-main.cpp')
-# on_k('mpiFCCpx 2d-mhd*.c 2d-mhd-main.cpp -o a.out -O3 -Kfast,parallel -Kocl -Klib -Koptmsg=2 -Karray_private -Kinstance=8 -Kdynamic_iteration -Kloop_fission -Kloop_part_parallel -Kloop_part_simd -Keval  -Kreduction -Ksimd=2')
+#on_k('mpiFCCpx 2d-mhd*.c 2d-mhd-main.cpp')
+on_k('mpiFCCpx 2d-mhd*.c 2d-mhd-main.cpp -o a.out -O3 -Kfast,parallel -Kocl -Klib -Koptmsg=2 -Karray_private -Kinstance=8 -Kdynamic_iteration -Kloop_fission -Kloop_part_parallel -Kloop_part_simd -Keval  -Kreduction -Ksimd=2')
 on_k('pjsub submit.sh')
