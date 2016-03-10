9bc65ce96a5a9d35bd486512f68635ec0707c06f
Takayuki Muranushi <muranushi@gmail.com>
2016-03-10 14:10:14 +0900

    Let incubator load config files

--- exe-src/incubator.hs
+++ exe-src/incubator.hs
@@ -3,20 +3,105 @@
 module Main where
 
 import           Cases (snakify)
-import           Control.Concurrent (threadDelay)
+import           Control.Concurrent
+import qualified Control.Exception as C
 import           Control.Lens
+import           Control.Monad.State
 import           Data.Aeson.TH
+import           Data.List (isPrefixOf)
+import           Data.Maybe
 import qualified Data.ByteString as BS
 import qualified Data.Yaml as Y
 import qualified Data.Yaml.Pretty as Y
 import           Data.Text.Lens (packed)
 import           GHC.IO.Exception (ExitCode)
 import           System.Directory
+import           System.Exit
 import           System.FilePath ((</>))
+import           System.FilePath.Lens
 import           System.IO
-import           System.IO.Temp (withSystemTempDirectory)
+import           System.IO.Error
+import           System.IO.Temp
+import           System.IO.Unsafe
 import           System.Process
+
 import           Formura.NumericalConfig
+
+----------------------------------------------------------------
+-- External Functions Utilities
+----------------------------------------------------------------
+
+cmd :: String -> IO ExitCode
+cmd str = do
+  hPutStrLn stderr str
+  system str
+
+-- copy remote file/local file/url from one another
+superCopy :: FilePath -> FilePath -> IO ()
+superCopy src dest = do
+  let isUrl = or [x `isPrefixOf` src | x <- ["http://", "https://", "ftp://"]]
+      go :: (String -> IO ()) -> IO ()
+      go k
+        | isUrl = withSystemTempFile "tmp" $ \fn h -> do
+            hClose h
+            cmd $ "wget " ++ src ++ " -O " ++ fn
+            k fn
+        | otherwise = k src
+  go $ \fn -> do
+    cmd $ unwords ["scp", fn, dest]
+    return ()
+
+writeYaml :: Y.ToJSON a => FilePath -> a -> IO ()
+writeYaml fn obj = BS.writeFile fn $ Y.encodePretty (Y.setConfCompare compare Y.defConfig) obj
+
+readYaml :: Y.FromJSON a => FilePath -> IO (Maybe a)
+readYaml fn = do
+  Y.decodeFileEither fn >>= \case
+    Left msg -> do
+      hPutStrLn stderr $ "When reading " ++ fn ++ "\n" ++ Y.prettyPrintParseException msg
+      return Nothing
+    Right x -> return $ Just x
+
+readCmd :: String -> IO String
+readCmd str = interactCmd str ""
+
+interactCmd
+    :: String                   -- ^ shell command to run
+    -> String                   -- ^ standard input
+    -> IO String                -- ^ stdout + stderr
+interactCmd cmdstr input = do
+    (Just inh, Just outh, _, pid) <-
+        createProcess (shell cmdstr){ std_in  = CreatePipe,
+                                      std_out = CreatePipe,
+                                      std_err = Inherit }
+
+    -- fork off a thread to start consuming the output
+    output  <- hGetContents outh
+    outMVar <- newEmptyMVar
+    forkIO $ C.evaluate (length output) >> putMVar outMVar ()
+
+    -- now write and flush any input
+    when (not (null input)) $ do hPutStr inh input; hFlush inh
+    hClose inh -- done with stdin
+
+    -- wait on the output
+    takeMVar outMVar
+    hClose outh
+
+    -- wait on the process
+    ex <- waitForProcess pid
+
+    case ex of
+     ExitSuccess   -> return output
+     ExitFailure r ->
+      error ("readSystem: " ++ cmdstr ++ " (exit " ++ show r ++ ")")
+
+
+
+----------------------------------------------------------------
+-- Incubator Datatypes
+----------------------------------------------------------------
+
 
 data Action = Codegen
             | Compile
@@ -37,7 +122,7 @@ data QBConfig =
   , _qbLabNotePath :: String
   }
 
-makeLenses ''QBConfig
+makeClassy ''QBConfig
 
 $(deriveJSON (let toSnake = packed %~ snakify in
                defaultOptions{fieldLabelModifier = toSnake . drop 3,
@@ -52,7 +137,7 @@ qbConfigFilePath = ".qb/config"
 qbDefaultConfig = QBConfig
   { _qbHostName = "greatwave"
   , _qbWorkDir = ".qb/work"
-  , _qbLabNotePath = "/home/nushio/hub/3d-mhd/log"}
+  , _qbLabNotePath = "/home/nushio/hub/3d-mhd/individuals"}
 
 type WithQBConfig = ?qbc :: QBConfig
 
@@ -71,7 +156,7 @@ data Individual =
   , _idvImagePath :: Maybe String
   }
 
-makeLenses ''Individual
+makeClassy ''Individual
 
 $(deriveJSON (let toSnake = packed %~ snakify in
                defaultOptions{fieldLabelModifier = toSnake . drop 4,
@@ -84,9 +169,9 @@ defaultIndividual = Individual
   { _idvAction = Codegen
   , _idvName = "default"
   , _idvFormuraVersion = "3aed540676dca114e9367ef1d94b0b3ca00ea8f4"
-  , _idvSourcecodeURL = "https://raw.githubusercontent.com/nushio3/3d-mhd/c1901c5c8c0ebf711f84e3fefbdeb44fd94c97b1/src/3d-mhd.fmr"
-  , _idvNumericalConfig = defaultNumericalConfig
-  , _idvLocalWorkDir = Nothing
+  , _idvSourcecodeURL = "/home/nushio/hub/formura/examples/3d-mhd.fmr"
+  , _idvNumericalConfig = unsafePerformIO $ fromJust <$> readYaml "/home/nushio/hub/formura/examples/3d-mhd.nc"
+  , _idvLocalWorkDir = Just "/home/nushio/hub/formura-rawdata/"
   , _idvLocalCodePaths = Nothing
   , _idvRemoteWorkDir = Nothing
   , _idvRemoteExecPath = Nothing
@@ -95,15 +180,31 @@ defaultIndividual = Individual
   }
 
 
+data IncubatorState =
+  IncubatorState
+  { _qbConfig :: QBConfig
+  , _qbIndividual :: Individual}
+
+makeClassy ''IncubatorState
+
+instance HasQBConfig IncubatorState where
+  qBConfig = qbConfig
+
+instance HasIndividual IncubatorState where
+  individual = qbIndividual
+
+
 ----------------------------------------------------------------
--- Utility Functions
+-- Incubator functions
 ----------------------------------------------------------------
 
-cmd :: String -> IO ExitCode
-cmd str = do
-  hPutStrLn stderr str
-  system str
-
+readIndividual :: FilePath -> IO (Maybe Individual)
+readIndividual fn = do
+  readYaml fn >>= \case
+    Nothing -> return Nothing
+    Just x -> return $ Just $ x
+      { _idvLocalWorkDir = Just $ fn ^. directory
+      }
 
 getCompiler :: WithQBConfig => String -> IO FilePath
 getCompiler gitKey = do
@@ -125,17 +226,15 @@ getCompiler gitKey = do
 
 codegen :: WithQBConfig => Individual -> IO Individual
 codegen idv = do
-  let git = ?qbc ^. qbLabNotePath
+  let labNote = ?qbc ^. qbLabNotePath
+      Just workDir = idv ^. idvLocalWorkDir
+  cmd $ "mkdir -p " ++ workDir
   codegenFn <- getCompiler $ idv ^. idvFormuraVersion
-  absPath <- getCurrentDirectory
-  withSystemTempDirectory "qb-codegen" $ \dir -> do
-    withCurrentDirectory dir $ do
-      cmd $ "wget " ++ idv ^. idvSourcecodeURL ++ " -O main.fmr"
-      writeYaml "main.nc" $ idv ^. idvNumericalConfig
-
-
-
-  return idv
+  withCurrentDirectory workDir $ do
+    superCopy (idv ^. idvSourcecodeURL) "main.fmr"
+    writeYaml "main.nc" $ idv ^. idvNumericalConfig
+    cmd $ codegenFn ++ " main.fmr"
+  return $ idv & idvAction .~ Compile
 
 compile :: Individual -> IO Individual
 compile idv = return idv
@@ -146,16 +245,6 @@ benchmark idv = return idv
 visualize :: Individual -> IO Individual
 visualize = return
 
-writeYaml :: Y.ToJSON a => FilePath -> a -> IO ()
-writeYaml fn obj = BS.writeFile fn $ Y.encodePretty (Y.setConfCompare compare Y.defConfig) obj
-
-readYaml :: Y.FromJSON a => FilePath -> IO a
-readYaml fn = do
-  Y.decodeFileEither fn >>= \case
-    Left msg -> do
-      hPutStrLn stderr $ "When reading " ++ fn
-      error $ Y.prettyPrintParseException msg
-    Right x -> return x
 
 main :: IO ()
 main = do
@@ -170,13 +259,20 @@ mainInit = do
 mainServer :: IO ()
 mainServer = do
   putStrLn "Qppy!"
-  qbc0 <- readYaml qbConfigFilePath
-  let ?qbc = qbc0
+  Just qbc0 <- readYaml qbConfigFilePath
+  let ?qbc = qbc0 :: QBConfig
+  let noteDir = ?qbc ^. qbLabNotePath
+  findIdvs <- readCmd $ "find " ++ noteDir ++ " -name '*.idv'"
+  let idvFns = lines findIdvs
 
-  writeYaml "izanagi.yaml" defaultIndividual
+  idvs <- catMaybes <$> mapM readIndividual idvFns
 
-  _ <- codegen $ defaultIndividual
+  mapM_ proceed idvs
 
+  return ()
+
+proceed :: Individual -> IO ()
+proceed idv = do
   return ()
 
 {- note: to submit interactive job on greatwave:
