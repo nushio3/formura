1c82911cf7001be3e23fb4a53c01841486d3eddb
Takayuki Muranushi <muranushi@gmail.com>
2016-03-17 12:28:37 +0900

    update  Validate script

--- scripts/cleanvalidate.sh
+++ scripts/cleanvalidate.sh
@@ -1,12 +1,9 @@
-stack clean
-make clean
-rm CMakeCache.txt
-stack build
+rm  examples/3d-mhd*.c
+stack exec formura -- examples/3d-mhd.fmr
 ./cmake-for-x86.sh
 # WARNING: you need to symbolic link the .bin folder
-stack exec formura -- examples/2d-mhd.fmr
-make
-mkdir -p out-2d-mhd
-rm out-2d-mhd/*
-./bin/2d-mhd.out 5000 100
-./scripts/plot-2d-mhd.py out-2d-mhd/*.txt
+make bin/3d-mhd.out -j8
+# mkdir -p out-2d-mhd
+# rm out-2d-mhd/*
+# ./bin/2d-mhd.out 5000 100
+# ./scripts/plot-2d-mhd.py out-2d-mhd/*.txt
