25160fb16c4ba288e1ed4314eb1d77ffd9dec76e
Takayuki Muranushi <muranushi@gmail.com>
2016-03-26 23:41:56 +0900

    VdP

--- scripts/lotka-volterra-test.py
+++ scripts/lotka-volterra-test.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python
 import pylab
 
-rU = 1.5, rV = 0.5, rE = 0.2
+mu = 0.1
+
 dt = 0.01
 
 U = 1.0
-V = 0.01
+V = 1.0
 
 us = []
 vs = []
 
 t = 0.0
 
-while t < 10.0:
-    dU_dt = -rE * U * V + rU * U * (1-U)
-    dV_dt =  rE * U * V - rV * V
+while t < 1000.0:
+    dU_dt = V
+    dV_dt = mu*(1-U*U)*V - U
     U += dt * dU_dt
     V += dt * dV_dt
     us.append(U)
     vs.append(V)
+    t += dt
 
 pylab.rcParams['figure.figsize'] = (10.0, 10.0)
 pylab.clf()
