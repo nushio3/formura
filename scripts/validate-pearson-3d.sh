86c3c93a60349ed5d2fa0b0dc8d52ae73e6ebd3e
Takayuki Muranushi <muranushi@gmail.com>
2016-03-29 17:57:52 +0900

    2 no beki ni suru

--- scripts/validate-pearson-3d.sh
+++ scripts/validate-pearson-3d.sh
@@ -1,5 +1,5 @@
 ./scripts/cleanvalidate.sh pearson-3d
 mkdir -p out images
 rm out/* images/*
- mpiexec -n 8 ./bin/pearson-3d.out 3200 200
+ mpiexec -n 8 ./bin/pearson-3d.out 3072 128
 ./scripts/plot-pearson-bin.py out/monitor-*.txt
