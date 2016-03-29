d156f9dc7adbbfff1b92bd256970c9de2fe8e4b8
Takayuki Muranushi <muranushi@gmail.com>
2016-03-29 15:10:48 +0900

    Test using temporal blocking.

--- scripts/validate-pearson-3d.sh
+++ scripts/validate-pearson-3d.sh
@@ -1,5 +1,5 @@
 ./scripts/cleanvalidate.sh pearson-3d
 mkdir -p out images
 rm out/* images/*
- mpiexec -n 8 ./bin/pearson-3d.out 30000 500
+ mpiexec -n 8 ./bin/pearson-3d.out 1000 100
 ./scripts/plot-pearson-bin.py out/monitor-*.txt
