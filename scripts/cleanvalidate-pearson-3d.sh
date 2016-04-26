0c60d63aaad12b8cd88ba57c3ccb36596f44d6a0
Takayuki Muranushi <muranushi@gmail.com>
2016-04-26 10:56:17 +0900

    Update the plotter

--- scripts/cleanvalidate-pearson-3d.sh
+++ scripts/cleanvalidate-pearson-3d.sh
@@ -1,6 +1,6 @@
 ./scripts/cleanvalidate.sh pearson-3d
 rm out/*
 rm images/*
-nice mpiexec -n 1 ./bin/pearson-3d.out 4000 256
+nice mpiexec -n 1 ./bin/pearson-3d.out 30000 256
 sleep 1
 ./scripts/plot-pearson-bin.py little out/*.txt
--- scripts/plot-2d-mhd.py
+++ scripts/plot-2d-mhd.py
@@ -15,13 +15,16 @@ for fn in fns[1:]:
             fp2.write(con.replace('nan','-1'))
     with open('scripts/plot.gnu', 'w') as fp:
         fp.write("""
-        set term png
+        set term png 26 size 1280,900
         set pm3d
         set pm3d map
         set size ratio -1
         set cbrange [-1:1]
         set out "{fn_out}"
-        splot "{fn_in}" u 1:2:3
+        set xrange [0:600]
+        set yrange [0:600]
+        set title 't = 6000'
+        splot "{fn_in}" u ($1-8):($2-8):3 t ''
 """.format(fn_in=fn_in, fn_out=fn_out))
 
     subprocess.call("gnuplot scripts/plot.gnu", shell=True)
