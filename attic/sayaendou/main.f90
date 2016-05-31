ffb2be7b6be22856307a89aa73728d28d2cbcc05
a03209 <a03209@fe01p02.k>
2016-05-31 10:40:36 +0900

    Implement formura fortran target code

--- attic/sayaendou/main.f90
+++ attic/sayaendou/main.f90
@@ -6,12 +6,14 @@ program main
   integer t
   call init()
 
-  call fapp_start("main", 0,0);
+  call fapp_start("main", 0,0)
+  call start_collection("kantai")
   do t = 1, 8*1024
      call proceed(u,v, u_other, v_other)
      call proceed(u_other,v_other, u, v)
   end do
-  call fapp_stop("main", 0,0);
+  call stop_collection("kantai")
+  call fapp_stop("main", 0,0)
 
   print *, u(25,25,25), v(25,25,25)
 
