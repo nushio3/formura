ef9ffc3a4fe992df4f632a5a70340585e2b8d161
Takayuki Muranushi <muranushi@gmail.com>
2016-06-22 16:47:32 +0900

    Write subroutines

---
+++ attic/fortran-linker/main.f90
@@ -0,0 +1,18 @@
+#include "parameter.h"
+program main
+  implicit none
+
+  integer t
+  call init()
+
+  call fapp_start("main", 0,0)
+  call start_collection("kantai")
+  do t = 1, 8*1024
+     call proceed(u,v, u_other, v_other)
+     call proceed(u_other,v_other, u, v)
+  end do
+  call stop_collection("kantai")
+  call fapp_stop("main", 0,0)
+
+  print *, u(25,25,25), v(25,25,25)
+end program main
---
+++ attic/fortran-linker/parameter.h
@@ -0,0 +1,3 @@
+#define NX 50
+#define NY 50
+#define NZ 50
