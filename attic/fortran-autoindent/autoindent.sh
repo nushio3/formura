d8632e3345edda5f7f0e1669ec646c11af23e7d7
Takayuki Muranushi <muranushi@gmail.com>
2016-06-24 15:39:36 +0900

    Autoindent test succeeded!

--- attic/fortran-autoindent/autoindent.sh
+++ attic/fortran-autoindent/autoindent.sh
@@ -1,3 +1,3 @@
 cp example.f90 example-indented.f90
-emacs --batch -l ~/emacs-batch-f90-indent.el \
+emacs --batch -l emacs-batch-f90-indent.el \
     -f f90-batch-indent-region example-indented.f90
