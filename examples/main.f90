18bfc3bfaf80e4cd372cf0125352a9f28f7c4005
Takayuki Muranushi <muranushi@gmail.com>
2016-06-28 18:36:00 +0900

    Output some more information

--- examples/main.f90
+++ examples/main.f90
@@ -6,6 +6,9 @@ program main
   type(Formura_Navigator) :: navi
   call Formura_Init(navi, MPI_COMM_WORLD)
   print *, "time step = ", navi%time_step
+  print *, "lower x = ", navi%lower_x
+  print *, "upper x = ", navi%upper_x
+
   call init()
 
 
