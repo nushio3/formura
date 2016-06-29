54344d758c818c91de62a68b83019333c109d6cc
a03209 <a03209@fe01p08.k>
2016-06-29 15:47:16 +0900

    Test some meaningful communication over MPI channel

--- attic/fortran-mpi-test/main.f90
+++ attic/fortran-mpi-test/main.f90
@@ -1,13 +1,25 @@
 program main
   implicit none
   include "mpif.h"
-  
-  integer :: mpi_err, mpi_req
+
+  integer :: i
+  integer :: mpi_err, req_recv, req_send
   double precision, dimension(42) :: U
+  double precision, dimension(42) :: V
 
   call mpi_init(mpi_err)
 
-  call mpi_irecv(U, 42*8, MPI_BYTE, 0, 0, mpi_comm_world, mpi_req ,mpi_err)
+  do i = 1, 42
+     U(i) = i*(i+1)
+  end do
+  
+  call mpi_irecv(V, 42*8, MPI_BYTE, 0, 0, mpi_comm_world, req_recv ,mpi_err)
+  call mpi_isend(U, 42*8, MPI_BYTE, 0, 0, mpi_comm_world, req_send ,mpi_err)
+  call mpi_wait(req_recv)
+
+  do i = 1, 42
+     print *, V(i)
+  end do
 
   call mpi_finalize(mpi_err)
 
