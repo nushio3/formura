e5ca87f8a522ec671a1a02e74fcdea95822a8d1a
Takayuki Muranushi <muranushi@gmail.com>
2016-06-29 09:51:51 +0900

    Init and finalize MPI

--- examples/main.f90
+++ examples/main.f90
@@ -4,6 +4,10 @@ program main
 
   implicit none
   type(Formura_Navigator) :: navi
+
+  print *, "Formura test main program."
+
+  call mpi_init(mpi_err)
   call Formura_Init(navi, MPI_COMM_WORLD)
   print *, "time step = ", navi%time_step
   print *, "lower x = ", navi%lower_x
@@ -12,6 +16,7 @@ program main
   call init()
 
 
+  call mpi_finalize(mpi_err)
 contains
   subroutine init()
 
