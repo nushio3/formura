817bc93d12f73955734f1ac6d3d9d5fb66497d2b
a03209 <a03209@fe01p08.k>
2016-06-29 16:07:35 +0900

    Solve the secret of sizeof

--- attic/fortran-mpi-test/main.f90
+++ attic/fortran-mpi-test/main.f90
@@ -3,15 +3,19 @@ program main
 
   implicit none
   include "mpif.h"
+  
+  integer :: size_var
 
   call mpi_init(mpi_err)
 
   do i = 1, 42
      Iriguchi%U(i) = i*(i+1)
   end do
-  
-  call mpi_irecv(Deguchi , sizeof(Deguchi), MPI_BYTE, 0, 0, mpi_comm_world, req_recv ,mpi_err)
-  call mpi_isend(Iriguchi, sizeof(Iriguchi), MPI_BYTE, 0, 0, mpi_comm_world, req_send ,mpi_err)
+
+  print *, "sizeof deguchi = ", sizeof(Deguchi)
+  size_var = sizeof(Deguchi)
+  call mpi_irecv(Deguchi , size_var, MPI_BYTE, 0, 0, mpi_comm_world, req_recv ,mpi_err)
+  call mpi_isend(Iriguchi, size_var, MPI_BYTE, 0, 0, mpi_comm_world, req_send ,mpi_err)
   call mpi_wait(req_recv, mpi_status_ignore,mpi_err)
 
   do i = 1, 42
