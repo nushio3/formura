ea90198e2bf63a3eb84b96fd6ed70eb5505af460
a03209 <a03209@fe01p08.k>
2016-06-29 15:53:44 +0900

    Use structure for communication

--- attic/fortran-mpi-test/main.f90
+++ attic/fortran-mpi-test/main.f90
@@ -4,21 +4,27 @@ program main
 
   integer :: i
   integer :: mpi_err, req_recv, req_send
-  double precision, dimension(42) :: U
-  double precision, dimension(42) :: V
+  type Kozo
+     double precision, dimension(42) :: U
+     double precision, dimension(42) :: V     
+  end type Kozo
+
+  type(Kozo) :: Iriguchi
+  type(Kozo) :: Deguchi
+
 
   call mpi_init(mpi_err)
 
   do i = 1, 42
-     U(i) = i*(i+1)
+     Iriguchi%U(i) = i*(i+1)
   end do
   
-  call mpi_irecv(V, 42*8, MPI_BYTE, 0, 0, mpi_comm_world, req_recv ,mpi_err)
-  call mpi_isend(U, 42*8, MPI_BYTE, 0, 0, mpi_comm_world, req_send ,mpi_err)
-  call mpi_wait(req_recv)
+  call mpi_irecv(Deguchi , 42*16, MPI_BYTE, 0, 0, mpi_comm_world, req_recv ,mpi_err)
+  call mpi_isend(Iriguchi, 42*16, MPI_BYTE, 0, 0, mpi_comm_world, req_send ,mpi_err)
+  call mpi_wait(req_recv, mpi_status_ignore,mpi_err)
 
   do i = 1, 42
-     print *, V(i)
+     print *, Deguchi%U(i)
   end do
 
   call mpi_finalize(mpi_err)
