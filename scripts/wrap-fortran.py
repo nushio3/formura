895c322d346c96966765b80a95fead1908f19c05
Takayuki Muranushi <muranushi@gmail.com>
2016-07-01 00:00:46 +0900

    newliner

--- scripts/wrap-fortran.py
+++ scripts/wrap-fortran.py
@@ -2,11 +2,25 @@
 
 import re,sys
 
+limit=250
+
 for l in sys.stdin:
-    if len(l) < 250:
+    if len(l) < limit:
         sys.stdout.write(l)
     else:
         ws = l.split()
-        line_end = "&"
+        line_end = " &"
         line_begin=""
-         if ws[0][0] != '!'
+        if re.search('^\!\$omp',ws[0]):
+            line_begin = "!$omp& "
+        elif re.search('^\!',ws[0]):
+            line_begin = "! "
+        buf = ""
+        for w in ws:
+            if len(buf+w)>=limit-3:
+                sys.stdout.write(buf+line_end+"\n")
+                buf=line_begin + w
+            else:
+                buf += " " + w
+        if buf != "":
+            sys.stdout.write(buf+"\n")
