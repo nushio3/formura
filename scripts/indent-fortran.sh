62e46338f5a26670d20a5060bdee43f94fa6bb38
Takayuki Muranushi <muranushi@gmail.com>
2016-07-01 00:05:16 +0900

    Wrapper

--- scripts/indent-fortran.sh
+++ scripts/indent-fortran.sh
@@ -1,1 +1,2 @@
 emacs --batch -l ./scripts/emacs-batch-f90-indent.el -f f90-batch-indent-region $1
+./scripts/wrap-fortran.py $1
--- scripts/wrap-fortran.py
+++ scripts/wrap-fortran.py
@@ -4,23 +4,30 @@ import re,sys
 
 limit=250
 
-for l in sys.stdin:
-    if len(l) < limit:
-        sys.stdout.write(l)
-    else:
-        ws = l.split()
-        line_end = " &"
-        line_begin=""
-        if re.search('^\!\$omp',ws[0]):
-            line_begin = "!$omp& "
-        elif re.search('^\!',ws[0]):
-            line_begin = "! "
-        buf = ""
-        for w in ws:
-            if len(buf+w)>=limit-3:
-                sys.stdout.write(buf+line_end+"\n")
-                buf=line_begin + w
-            else:
-                buf += " " + w
-        if buf != "":
-            sys.stdout.write(buf+"\n")
+for fn in argv[1:]:
+    with open(fn,"r") as fp:
+        con = fp.read()
+
+    ret = ""
+    for l in con.split("\n"):
+        if len(l) < limit:
+            ret += l + "\n"
+        else:
+            ws = l.split()
+            line_end = " &"
+            line_begin=""
+            if re.search('^\!\$omp',ws[0]):
+                line_begin = "!$omp& "
+            elif re.search('^\!',ws[0]):
+                line_begin = "! "
+            buf = ""
+            for w in ws:
+                if len(buf+w)>=limit-3:
+                    ret += buf+line_end+"\n"
+                    buf=line_begin + w
+                else:
+                    buf += " " + w
+            if buf != "":
+                ret += buf+"\n"
+    with open(fn,"w") as fp:
+        fp.write(ret)
