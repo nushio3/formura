878c87019296a00b08c261dabfa67aa4d3be5d55
Takayuki Muranushi <muranushi@gmail.com>
2016-07-06 14:46:55 +0900

    Visualization script

--- scripts/cleanvalidate-K.sh
+++ scripts/cleanvalidate-K.sh
@@ -3,10 +3,3 @@ stack install
 stack exec formura -- examples/finalist.fmr -o examples/finalist.f90
 (cd examples/; ../bin/make-make)
 rsync -avz ./examples/Makefile examples/*.f90 K:/volume73/data/ra000008/nushio/formura/attic/f-test
-#./cmake-for-x86.sh
-#make -j8 bin/$1.out
-# WARNING: you need to symbolic link the .bin folder
-# mkdir -p out-2d-mhd
-# rm out-2d-mhd/*
-# ./bin/2d-mhd.out 5000 100
-# ./scripts/plot-2d-mhd.py out-2d-mhd/*.txt
---
+++ scripts/visualize-K.sh
@@ -0,0 +1,1 @@
+rsync -avz K:/volume73/data/ra000008/nushio/formura/attic/f-test/*.bin images-K/
