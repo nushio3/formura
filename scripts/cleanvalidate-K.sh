c8d9427e417ca365d46bb92004f17628a651bee2
Takayuki Muranushi <muranushi@gmail.com>
2016-07-06 11:18:25 +0900

    Remove extra files

--- scripts/cleanvalidate-K.sh
+++ scripts/cleanvalidate-K.sh
@@ -1,7 +1,8 @@
 rm  examples/finalist*.f90 examples/Makefile bin/finalist.out examples/*internal*
 stack install
 stack exec formura -- examples/finalist.fmr -o examples/finalist.f90
-rsync -avz examples/*.f90 K:/volume73/data/ra000008/nushio/formura/attic/f-test
+(cd examples/; ../bin/make-make)
+rsync -avz ./examples/Makefile examples/*.f90 K:/volume73/data/ra000008/nushio/formura/attic/f-test
 #./cmake-for-x86.sh
 #make -j8 bin/$1.out
 # WARNING: you need to symbolic link the .bin folder
