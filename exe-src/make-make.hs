d3a219d23297be53d32c186033c09a75887f4723
Takayuki Muranushi <muranushi@gmail.com>
2016-07-07 11:36:57 +0900

    make-make adopted to handle both Fortran and C.

--- exe-src/make-make.hs
+++ exe-src/make-make.hs
@@ -1,12 +1,23 @@
+{-# LANGUAGE MultiWayIf #-}
 import           Control.Lens
 import           Data.List
 import           System.FilePath.Lens
+import           System.Environment
+
 import           Formura.Utilities
 
 
 
 main :: IO ()
 main = do
+  argv <- getArgs
+
+  if | "-F" `elem` argv -> mainF
+     | "-C" `elem` argv -> mainC
+     | otherwise        -> error "give me -F or -C"
+
+mainF :: IO ()
+mainF = do
   foundFiles <- fmap (sort . lines) $ readCmd $ "find . | grep finalist"
   let csrcFiles =
         [fn | fn <- foundFiles, fn ^. extension == ".f90", fn ^.basename /= "main"]
@@ -28,7 +39,27 @@ main = do
         , "\t$(CC) -c $^ -o $@ 2> $@.optmsg"]
   writeFile "Makefile" $ unlines
     [ "all: a.out"
-    , "CC=mpifrtpx -Cpp  -Karray_private -Kdynamic_iteration -Keval -Kfast,parallel -Kinstance=8 -Klib -Kloop_fission -Kloop_part_parallel -Kloop_part_simd -Kocl -Kopenmp -Kparallel -Kpreex -Kreduction -Ksimd=2"
+    , "CC=mpifrtpx -Cpp  -Karray_private -Kdynamic_iteration -Keval -Kfast,parallel -Kinstance=8 -Kloop_fission -Kloop_part_parallel -Kloop_part_simd -Kocl -Kopenmp -Kparallel -Kpreex -Kreduction -Ksimd=2"
+    , "OBJS=" ++ unwords objFiles
+    , "a.out: $(OBJS)"
+    , "\t$(CC) $(OBJS) -o a.out"
+    , unlines $ map c2oCmd csrcFiles]
+
+mainC :: IO ()
+mainC = do
+  foundFiles <- fmap (sort . lines) $ readCmd $ "find . | grep champion"
+  let csrcFiles =
+        [fn | fn <- foundFiles, (fn ^. extension) `elem` [".c", ".cpp"] ]
+      objFiles = [fn & extension .~ "o"  |fn <- csrcFiles]
+
+      dependencyOf _ = []
+
+      c2oCmd fn = unlines
+        [ (fn & extension .~ "o") ++ ": " ++ unwords (fn: dependencyOf fn)
+        , "\t$(CC) -c $^ -o $@ 2> $@.optmsg"]
+  writeFile "Makefile" $ unlines
+    [ "all: a.out"
+    , "CC=mpiFCCpx -Cpp  -Karray_private -Kdynamic_iteration -Keval -Kfast,parallel -Kinstance=8 -Klib -Kloop_fission -Kloop_part_parallel -Kloop_part_simd -Kocl -Kopenmp -Kparallel -Kpreex -Kreduction -Ksimd=2"
     , "OBJS=" ++ unwords objFiles
     , "a.out: $(OBJS)"
     , "\t$(CC) $(OBJS) -o a.out"
--- scripts/cleanvalidate-F.sh
+++ scripts/cleanvalidate-F.sh
@@ -1,5 +1,5 @@
 rm  examples/finalist*.f90 examples/Makefile bin/finalist.out examples/*internal*
 stack install
 stack exec formura -- examples/finalist.fmr -o examples/finalist.f90
-(cd examples/; ../bin/make-make)
+(cd examples/; ../bin/make-make -F)
 rsync -avz ./examples/Makefile examples/*.f90 K:/volume73/data/ra000008/nushio/formura/attic/f-test
