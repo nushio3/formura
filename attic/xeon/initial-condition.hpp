e79c023eb6a343b9f7628bee2c6de66ecab02adb
Takayuki Muranushi <muranushi@gmail.com>
2016-08-19 10:04:38 +0900

    Write function to set up initial conditions.

---
+++ attic/xeon/initial-condition.hpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+
+template <int NX, int NY, int NZ, class T> void fill_initial_condition(T U[NX][NY][NZ], T V[NX][NY][NZ]) {
+  for (int x=0;x<NX;++x) {
+    for (int y=0;y<NY;++y) {
+      for (int z=0;z<NZ;++z) {
+        U[x][y][z] = 1;
+        V[x][y][z] = 0;
+      }
+    }
+  }
+  int bx = std::max(NX/4,NX/2-8),  ex = std::min(3*NX/4+1,NX/2+8);
+  int by = std::max(NY/4,NY/2-8),  ey = std::min(3*NY/4+1,NY/2+8);
+  int bz = std::max(NZ/4,NZ/2-8),  ez = std::min(3*NZ/4+1,NZ/2+8);
+  for (int x=bx;x<ex;++x){
+    for (int y=by;y<ey;++y){
+      for (int z=bz;z<ez;++z){
+        U[x][y][z] = 0.5;
+        V[x][y][z] = 0.25;
+      }
+    }
+  }
+}
--- attic/xeon/main.cpp
+++ attic/xeon/main.cpp
@@ -1,6 +1,13 @@
-void a();
+#include "initial-condition.hpp"
+
+#define NX 1
+#define NY 50
+#define NZ 50
+
+double U[NX][NY][NZ];
+double V[NX][NY][NZ];
+
 
 int main () {
-
-  a();
+  fill_initial_condition<NX,NY,NZ,double>(U,V);
 }
