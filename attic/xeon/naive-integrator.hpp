76991da5356b83fa21e33e23e1970377401ef44b
Takayuki Muranushi <muranushi@gmail.com>
2016-08-19 12:59:37 +0900

    Confirmed a valid periodic-boudary access

--- attic/xeon/naive-integrator.hpp
+++ attic/xeon/naive-integrator.hpp
@@ -1,9 +1,8 @@
-#include <algorithm>
 
 template <int NX, int NY, int NZ, class T> struct naive_integrator {
   typedef T surface[NX][NY];
-  static T U_other[NX][NY][NZ], V_other[NX][NY][NZ];
   void proceed(int timesteps, T U[NX][NY][NZ], T V[NX][NY][NZ]) {
+    static T U_other[NX][NY][NZ], V_other[NX][NY][NZ];
     for (int t=0;t<timesteps;++t){
       auto wrap = [](T ar[NX][NY][NZ],int x, int y, int z) {
         x = (x+NX)%NX;
@@ -18,8 +17,20 @@ template <int NX, int NY, int NZ, class T> struct naive_integrator {
           }
         }
       }
-      std::swap(U,U_other);
-      std::swap(V,V_other);
+      for (int x=0;x<NX;++x) {
+        for (int y=0;y<NY;++y) {
+          for (int z=0;z<NZ;++z) {
+            U[x][y][z]=U_other[x][y][z];
+          }
+        }
+      }
+      for (int x=0;x<NX;++x) {
+        for (int y=0;y<NY;++y) {
+          for (int z=0;z<NZ;++z) {
+            V[x][y][z]=V_other[x][y][z];
+          }
+        }
+      }
     }
   }
 };
